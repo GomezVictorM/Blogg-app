3005c9cb4c85fa19e4a349efe8a5071b6aea7ebc
GomezVictorM <victor.gomcha@gmail.com>
2020-07-15 14:49:40 +0200

    articleRepository findAll method

---
+++ src/main/java/gomez/victor/bloggapp/service/PostService.java
@@ -0,0 +1,19 @@
+package gomez.victor.bloggapp.service;
+
+import gomez.victor.bloggapp.entities.Article;
+import gomez.victor.bloggapp.repositories.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PostService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<Article> getAllArticles(){
+        return articleRepository.findAll();
+    }
+}
