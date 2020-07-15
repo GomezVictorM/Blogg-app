78dc607120eed175021a19bbab6f2d49bdb57a3d
GomezVictorM <victor.gomcha@gmail.com>
2020-07-15 14:32:29 +0200

    Entity Article created. Constructor and getters and setters

--- src/main/java/entities/Article.java
+++ src/main/java/entities/Article.java
@@ -1,6 +1,7 @@
 package entities;
 
 import javax.persistence.*;
+import java.util.Date;
 
 
 // Article = {
@@ -13,13 +14,45 @@ import javax.persistence.*;
 @Entity
 public class Article {
     @Id
-    @GeneratedValue(Strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String title;
     private String content;
-    private Long published;
+    private Date published;
 
+    public Article() {
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
 }
