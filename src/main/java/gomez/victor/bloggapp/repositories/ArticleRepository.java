9dc244668ed8583d4fae765c95546cba6cf9f630
GomezVictorM <victor.gomcha@gmail.com>
2020-07-15 15:12:25 +0200

    interface ArticleRepository, annotation @Repository added

--- src/main/java/gomez/victor/bloggapp/repositories/ArticleRepository.java
+++ src/main/java/gomez/victor/bloggapp/repositories/ArticleRepository.java
@@ -2,9 +2,11 @@ package gomez.victor.bloggapp.repositories;
 
 import gomez.victor.bloggapp.entities.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     List<Article> findById(Long id);
