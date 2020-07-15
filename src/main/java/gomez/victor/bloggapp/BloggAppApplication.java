5cd009fad8a9fcf4ca41c0983fe000df28288496
GomezVictorM <victor.gomcha@gmail.com>
2020-07-15 12:25:17 +0200

    First commit

---
+++ src/main/java/gomez/victor/bloggapp/BloggAppApplication.java
@@ -0,0 +1,14 @@
+package gomez.victor.bloggapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BloggAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gomez/victor/bloggapp/BloggAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gomez.victor.bloggapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
