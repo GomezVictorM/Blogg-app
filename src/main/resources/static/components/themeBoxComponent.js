bb69a8e4f18e867a33c6ad019f6a03c6893ba7da
GomezVictorM <victor.gomcha@gmail.com>
2020-07-23 19:13:24 +0200

    Theme box vue component created, not already working property. Trying to make it "talk" with other components

---
+++ src/main/resources/static/components/themeBoxComponent.js
@@ -0,0 +1,59 @@
+import createChannel from './createTheme.js'
+
+export default {
+    components: {
+        createTheme
+    },
+    template: `
+        <div class="theme-box-div">
+            <h2>Channels</h2>
+            <div v-for="(userTheme, i) in userThemes" :key="userTheme.id" class="theme-name-div">
+                <h3 @click="onClick(i)">{{userTheme.title}}</h3>
+                <button @click="onClickLeaveTheme(i)" class="delete-button">🗑️</button>
+            </div>
+            <createTheme/>
+        </div>
+    `,
+    computed: {
+        userThemes() {
+            if(this.$store.state.currentUser === null) {
+                return {}
+            } else {
+                return this.$store.state.currentUser.listOfThemes
+            }
+        },
+        showUserName(){
+            if(this.$store.state.currentUser === null) {
+               return ''
+            } else {
+                return this.$store.state.currentUser.username
+            }
+        }
+    },
+    methods: {
+        async onClick(i) {
+            this.$store.commit('setCurrentTheme', this.$store.state.currentUser.listOfThemes[i])
+            let channelId = this.$store.state.currentChannel.id
+            let response = await fetch('/rest/articles/' + themeId)
+            response = await response.json()
+            this.$store.commit('setCurrentThemeArticles', response)
+        },
+        async onClickLeaveChannel(i) {
+            let userThemeRelation = {
+                userId: this.$store.state.currentUser.id,
+                channelId: this.$store.state.currentUser.listOfThemes[i].id
+            }
+            console.log(userChannelRelation.userId)
+            console.log(userChannelRelation.channelId)
+            let response = await fetch('/rest/relation', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userThemeRelation)
+            })
+
+            this.$store.commit('removeThemeFromUser', i)
+        }
+    }
+}
