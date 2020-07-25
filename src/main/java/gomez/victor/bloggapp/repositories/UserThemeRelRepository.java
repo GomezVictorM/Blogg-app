076931d1e328baa2afd0f5df35b3fb5ceb1e07b1
GomezVictorM <victor.gomcha@gmail.com>
2020-07-25 23:36:11 +0200

    Interface UserThemeRelRepo

---
+++ src/main/java/gomez/victor/bloggapp/repositories/UserThemeRelRepository.java
@@ -0,0 +1,15 @@
+package gomez.victor.bloggapp.repositories;
+
+import gomez.victor.bloggapp.entities.UserTheme;
+import gomez.victor.bloggapp.entities.UserThemeRel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserThemeRelRepository extends CrudRepository<UserThemeRel, Integer> {
+    public List<UserThemeRel> findByUserId(int userId);
+    public void deleteByUserIdAndChannelId(@Param("userId") int userId, @Param("channelId") int channelId);
+}
